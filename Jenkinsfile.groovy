b5a9d0b53d16cbd8db25e83f49e6139ae8b62706
cuiyishuo <haizeier@gmail.com>
2019-07-31 17:07:39 +0800

    同步本地

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,47 +1,31 @@
 pipeline {
-  agent {
-    docker {
-      image 'maven:3-alpine'
-      args '-v /Users/cuiyishuo/.m2:/root/.m2'
-    }
-
-  }
-  stages {
-    stage('Build') {
-      steps {
-        sh 'mvn -B -DskipTests clean package'
-      }
-<<<<<<< HEAD
-      stage('Move jar') {
-                  steps {
-                      sh 'mv /compass'
-                  }
-                  post {
-                      always {
-                          echo "finish"
-                      }
-                  }
-            }
-   }
-=======
-    }
-    stage('Test') {
-      post {
-        always {
-          echo 'finish'
-
+    agent {
+        docker {
+            image 'maven:3-alpine'
+            args '-v /Users/cuiyishuo/.m2:/root/.m2'
         }
->>>>>>> 0de316afa06d88f17d1498c4cd2b3621da24788f
-
-      }
-      steps {
-        sh 'mvn test'
-      }
     }
-    stage('Biuld Image') {
-      steps {
-        sh 'mv /compass-web/target/*.jar /docker/app.jar'
-      }
+    stages {
+        stage('Build') {
+            steps {
+                sh 'mvn -B -DskipTests clean package'
+            }
+        }
+        stage('Test') {
+            steps {
+                sh 'mvn test'
+            }
+            post {
+                always {
+                    junit 'target/surefire-reports/*.xml'
+                }
+            }
+        }
+        stage('Deliver') {
+            steps {
+                sh './jenkins/scripts/deliver.sh'
+                input 'deliver Done'
+            }
+        }
     }
-  }
 }
